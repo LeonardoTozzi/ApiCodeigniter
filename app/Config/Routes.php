3f0292052f9763d8bf8c001775d6ebebb4520764
LeonardoTozzi <leonardotozzis@gmail.com>
2024-01-23 08:58:46 -0300

    metodos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/get-users', 'ApiController::getUsers');
+$routes->get('/update-user', 'ApiController::updateUser');
+$routes->get('/delete-user', 'ApiController::deleteUser');
+$routes->get('/create-user', 'ApiController::createUser');
--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -2,13 +2,94 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
-
 class ApiController extends BaseController
 {
-    public function index()
+    public function getUsers()
     {
-        //
+        $client = \Config\Services::curlrequest();
+        $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/users');
+
+        echo $response->getBody();
+      
     }
+
+    public function updateUser()
+{
+    $id = 1;
+    $client = \Config\Services::curlrequest();
+
+    // Faz a requisição PUT
+    $response = $client->request('PUT', 'https://jsonplaceholder.typicode.com/users/'.$id, [
+        'json' => [
+            'name' => 'John Doe',
+            'email' => 'Johndoe@gmail.com'
+        ]
+    ]);
+
+    // Verifica se a requisição foi bem-sucedida (código de status 200)
+    if ($response->getStatusCode() === 200) {
+
+        // Retorna os dados atualizados na resposta da API
+        return 'Usuário atualizado com sucesso: ';
+    } else {
+        // Se a requisição não foi bem-sucedida, retorne uma resposta de err     
+        echo 'Falha ao atualizar usuário: ';
+    }
+}
+ public function deleteUser()
+ {
+    $id = 1;
+    $client = \Config\Services::curlrequest();
+
+    // Faz a requisição DELETE
+    $response = $client->request('DELETE', 'https://jsonplaceholder.typicode.com/users/'.$id);
+
+    // Verifica se a requisição foi bem-sucedida (código de status 200)
+    if ($response->getStatusCode() === 200) {
+
+        // Retorna os dados atualizados na resposta da API
+        return 'Usuário deletado com sucesso: ';
+    } else {
+        // Se a requisição não foi bem-sucedida, retorne uma resposta de err     
+        echo 'Falha ao deletar usuário: ';
+    }
+ }
+
+ public function createUser()
+{
+    $client = \Config\Services::curlrequest();
+    // Faz a requisição POST
+    $response = $client->request('POST', 'https://jsonplaceholder.typicode.com/users', [
+        'json' => [
+            'name' => 'Leanne Graham',
+            'username' => 'Bret',
+            'email' => 'Sincere@april.biz',
+            'address' => [ 
+                'street' => 'Kulas Light',
+                'suite' => 'Apt. 556',
+                'city' => 'Gwenborough',
+                'zipcode' => '92998-3874',
+                'geo' => [
+                    'lat' => '-37.3159',
+                    'lng' => '81.1496'
+                ]
+            ],
+            'phone' => '1-770-736-8031 x56442',
+            'website' => 'hildegard.org',
+            'company' => [
+                'name' => 'Romaguera-Crona',
+                'catchPhrase' => 'Multi-layered client-server neural-net',
+                'bs' => 'harness real-time e-markets'
+            ]
+        ]
+    ]);
+      // Verifica se a requisição foi bem-sucedida (código de status 201 - Created)
+      if ($response->getStatusCode() === 201) {
+        // Retorna uma mensagem de sucesso
+        return 'Usuário criado com sucesso';
+    } else {
+        // Se a requisição não foi bem-sucedida, retorne uma resposta de erro
+        return 'Falha ao criar usuário';
+    }
+}
 }
